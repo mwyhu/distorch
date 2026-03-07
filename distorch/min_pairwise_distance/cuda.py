e9c65d519a18af1761b872e0025e2aed4e83b38c
Jérôme Rony <jerome@rony.fr>
2026-03-07 11:32:40 -0500

    Raise exceptions when conditions are not met

--- distorch/min_pairwise_distance/cuda.py
+++ distorch/min_pairwise_distance/cuda.py
@@ -65,8 +65,14 @@ def _minimum_sqdistances_3d(x1_ptr,
 
 
 def min_sqdist(x1: Tensor, x2: Tensor, BLOCK_SIZE: int = 2048) -> Tensor:
+    if not x1.is_cuda or x1.device != x2.device:
+        raise ValueError('x1 and x2 must be on the same CUDA device.')
     d = x1.size(1)
-    assert d == x2.size(1)
+    if d != x2.size(1):
+        raise ValueError(f'{x1.size(1)} and {x2.size(1)} must be equal.')
+    if not 2 <= d <= 3:
+        raise ValueError('d must be between 2 and 3.')
+
     n, m = x1.size(0), x2.size(0)
     BLOCK_SIZE = min(BLOCK_SIZE, triton.next_power_of_2(m))
     grid_cols = triton.cdiv(m, BLOCK_SIZE)
