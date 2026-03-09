0556a03fa3c2a39be7d31a4de7360082c8ccc364
Jérôme Rony <jerome@rony.fr>
2026-03-09 07:17:16 -0400

    Add tests for minimum_distances

---
+++ tests/test_min_pairwise_distance.py
@@ -0,0 +1,25 @@
+import pytest
+import torch
+
+from distorch.min_pairwise_distance import minimum_distances
+
+devices = ["cpu"]
+if torch.cuda.is_available():
+    devices.append("cuda")
+
+
+@pytest.mark.parametrize("device_type", devices)
+@pytest.mark.parametrize("d", (2, 3))
+@pytest.mark.parametrize("dtype", (torch.float, torch.double))
+@pytest.mark.parametrize("n,m", ((3, 5), (64, 64), (64, 128), (128, 64)))
+def test_minin_sqdist(device_type: str, d: int, dtype: torch.dtype, n: int, m: int) -> None:
+    device = torch.device(device_type)
+
+    t1 = torch.randn(n, d, device=device, dtype=dtype)
+    t2 = torch.randn(m, d, device=device, dtype=dtype)
+
+    distances = torch.cdist(t1, t2, p=2)
+    min_distances_ref = distances.amin(dim=1).square()
+
+    min_distances = minimum_distances(t1, t2, sqrt=False)
+    torch.testing.assert_close(min_distances, min_distances_ref)
