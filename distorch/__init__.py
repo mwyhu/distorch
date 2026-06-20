c6d0af59597473ff7cb9c0d6ebb4730f9a7b237e
Jérôme Rony <jerome@rony.fr>
2026-06-20 11:13:55 -0400

    Bump version

--- distorch/__init__.py
+++ distorch/__init__.py
@@ -2,4 +2,4 @@ from torch.torch_version import TorchVersion
 
 from .metrics import boundary_metrics, pixel_center_metrics, overlap_metrics
 
-__version__ = TorchVersion('0.1.3')  # allows version comparison with strings
+__version__ = TorchVersion('0.1.4')  # allows version comparison with strings
